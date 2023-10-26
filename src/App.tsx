3b044534d2f5e98e63b3ad1f0106fdbb84026239
Jamesfriend052@gmail.com <Jamesfriend052@gmail.com>
2023-10-26 11:48:59 +0630

    version 3.0

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Header from "./components/global/header/Header";
 import Charts from "./components/global/main-charts/Charts";
 import { useTheme } from "./themeContext";
 function App() {
-  const {theme} = useTheme()
   return (
     <div className=" h-screen bg-slate-900">
           <div className="bg-zinc-700 w-[80%] border-l border-ring border-r border-r-ring mx-auto h-full " >
